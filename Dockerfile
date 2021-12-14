c44cb547cb32899c9734c9d717f551bcc4fc610a
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-14 21:13:32 +0200

    Tryna push app to heroku

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ EXPOSE 3001
 
 RUN npm install
 
-CMD [ "npm", "start" ] -p $PORT
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.post("/api/boardgames", (request, response) => {
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT | 3001
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
