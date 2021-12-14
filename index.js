c7b98e9ed67a8eac2aa48f830d431c53e960ef96
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-14 22:49:29 +0200

    Testing changes

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ app.post("/api/boardgames", (request, response) => {
     response.json(boardgame)
 })
 
-const PORT = process.env.PORT | 3001
-
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
