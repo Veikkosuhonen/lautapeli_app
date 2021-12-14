bedc4bb02e641d35675b38d4e72300d28eadc086
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-14 22:43:07 +0200

    Testing changes

--- index.js
+++ index.js
@@ -3,83 +3,29 @@ const app = express()
 
 app.use(express.json())
 
-
-let notes = [
+let boardgames = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2020-01-10T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2020-01-10T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2020-01-10T19:20:14.298Z",
-        important: true
+        "id": 0,
+        "content": "Hello"
     }
 ]
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>")
+app.get("/", (request, response) => {
+    response.send("<h1>Lautapelit!</h1>")
 })
 
-app.get("/api/notes", (req, res) => {
-    res.json(notes)
+app.get("/api/boardgames", (request, response) => {
+    response.json(boardgames)
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post("/api/notes", (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ 
-            error: "content missing" 
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
+app.post("/api/boardgames", (request, response) => {
+    const boardgame = request.body
+    response.json(boardgame)
 })
 
-app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT | 3001
 
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
-
-app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-})
-
-const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
