8ea221bdd04949dd03adc7f6dcd7ab7887b3a3d8
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-14 22:17:49 +0200

    Set host

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.post("/api/boardgames", (request, response) => {
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT | 3001
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", 511, () => {
     console.log(`Server running on port ${PORT}`)
 })
