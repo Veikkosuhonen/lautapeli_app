e89ef77ecfcb396ce8822be1373e646a538d1129
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-14 22:39:47 +0200

    Pasted stuff

--- index.js
+++ index.js
@@ -1,36 +1,85 @@
 const express = require("express")
-const cors = require("cors")
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 
-let boardgames = [
+
+let notes = [
     {
-        "id": 0,
-        "content": "Hello"
+        id: 1,
+        content: "HTML is easy",
+        date: "2020-01-10T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2020-01-10T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2020-01-10T19:20:14.298Z",
+        important: true
     }
 ]
 
-app.get("/", (request, response) => {
-    console.log("GET " + request.url)
-    response.send("<h1>Lautapelit!</h1>")
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>")
 })
 
-app.get("/api/boardgames", (request, response) => {
-    console.log("GET " + request.url)
-    response.json(boardgames)
+app.get("/api/notes", (req, res) => {
+    res.json(notes)
 })
 
-app.post("/api/boardgames", (request, response) => {
-    const boardgame = request.body
-    console.log(boardgame)
-    response.json(boardgame)
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({ 
+            error: "content missing" 
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT | 3001
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
 
-app.listen(PORT, "0.0.0.0", 511, () => {
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
