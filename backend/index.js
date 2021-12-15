4c9e1cd47b3102afef80598db828bfd59f9ca108
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-15 15:59:22 +0200

    Added sequelize postgres

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+const { Sequelize, Model, DataTypes } = require("sequelize")
 const express = require("express")
 const cors = require("cors")
 
@@ -6,40 +8,80 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static("build"))
 
-let boardgames = [
-    {
-        id: 0,
-        name: "Hello",
-    }
-]
 
-app.get("/api/boardgames", (request, response) => {
+
+
+// eslint-disable-next-line no-undef
+let dbUrl = process.env.DATABASE_URL
+
+if (!dbUrl) {
+    // eslint-disable-next-line no-undef
+    dbUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}/${process.env.POSTGRES_DATABASE}`
+}
+
+const sequelize = new Sequelize(dbUrl, {
+    dialect: "postgres",
+    dialectOptions: {
+        
+    },
+})
+
+
+class Boardgame extends Model {}
+Boardgame.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    timesPlayed: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    dateAdded: {
+        type: DataTypes.DATE
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "boardgame"
+})
+Boardgame.sync()
+
+
+
+
+app.get("/api/boardgames", async (request, response) => {
     //console.log("GET " + request.url)
-    response.json(boardgames)
+    const bgs = await Boardgame.findAll()
+    response.json(bgs)
 })
 
 const validateBoardgame = (body) => {
     return body.name !== undefined
 }
 
-const addBoardgame = (body) => {
-    let boardgame = {
-        id: boardgames.length,
-        name: body.name
-    }
-
-    boardgames.push(boardgame)
-    return boardgame
+const addBoardgame = async (body) => {
+    return await Boardgame.create({ name: body.name })
 }
 
-app.post("/api/boardgames", (request, response) => {
+app.post("/api/boardgames", async (request, response) => {
     const boardgame = request.body
     if (!validateBoardgame(boardgame)) {
         response.sendStatus(405)
     } else {
-        response.json(addBoardgame(boardgame))
+        const bg = await addBoardgame(boardgame)
+        response.json(bg)
     }
 })
+
+
+
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT
