da2006501673a1457e1ffea2655f2cc6c42fabea
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-28 17:05:11 +0200

    Requestlogger does not log bodies with password field

--- backend/src/middleware/requestLogger.js
+++ backend/src/middleware/requestLogger.js
@@ -3,7 +3,11 @@ const logger = require("../util/logger")
 const requestLogger = (request, response, next) => {
     logger.info(`${request.method} ${request.path}`)
     if (request.body) {
-        logger.info("   ", request.body)
+        if (request.body.password || request.body.token) {
+            logger.info("    { credentials! }")
+        } else {
+            logger.info("   ", request.body)
+        }
     }
     next()
 }
