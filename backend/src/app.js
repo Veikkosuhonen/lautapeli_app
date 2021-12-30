32e7c5623248a3bb1c121c8ceea9d971c6bca965
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-30 16:09:01 +0200

    Added middleware to force redirect to https

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,34 @@
 const express = require("express")
 const cors = require("cors")
 const { connectToDatabase } = require("./util/db")
-const requestLogger = require("./middleware/requestLogger")
-const errorHandler = require("./middleware/errorHandler")
+
+const requestLogger = require("./middleware/requestLogger"),
+    errorHandler = require("./middleware/errorHandler"),
+    requireHTTPS = require("./middleware/requireHTTPS")
+
+const bgRouter = require("./controllers/boardgames"),
+    playsessionRouter = require("./controllers/playSessions"),
+    usersRouter = require("./controllers/users"),
+    registerRouter = require("./controllers/register"),
+    loginRouter = require("./controllers/login"),
+    profileRouter = require("./controllers/myprofile"),
+    adminRouter = require("./controllers/admin"),
+    unknownEndpointRouter = require("./controllers/unknownEndpoint")
+
 const app = express()
 
 connectToDatabase().then(() => {
     app.emit("dbReady")
 })
 
+app.use(requireHTTPS)
+app.use(cors())
 app.use(express.static("build"))
 app.use(express.json())
-app.use(cors())
 
 app.use(requestLogger)
 
-const bgRouter = require("./controllers/boardgames")
-const playsessionRouter = require("./controllers/playSessions")
-const usersRouter = require("./controllers/users")
-const registerRouter = require("./controllers/register")
-const loginRouter = require("./controllers/login")
-const profileRouter = require("./controllers/myprofile")
-const adminRouter = require("./controllers/admin")
-const unknownEndpointRouter = require("./controllers/unknownEndpoint")
+
 
 app.use("/api/boardgames", bgRouter)
 app.use("/api/playsessions", playsessionRouter)
---
+++ backend/src/middleware/requireHTTPS.js
@@ -0,0 +1,13 @@
+const { NODE_ENV } = require("../util/config")
+
+// https://stackoverflow.com/questions/8605720/how-to-force-ssl-https-in-express-js
+
+const requireHTTPS = (request, response, next) => {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!request.secure && request.get('x-forwarded-proto') !== 'https' && NODE_ENV !== "test") {
+        return response.redirect('https://' + request.get('host') + request.url);
+    }
+    next();
+}
+
+module.exports = requireHTTPS
