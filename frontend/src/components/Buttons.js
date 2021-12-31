70373d393d9df091d121ccd1bcbb426b3110f8e4
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-31 16:01:57 +0200

    Improved register form

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -3,10 +3,13 @@ import React from "react"
 const PrimaryButton = ({
     text,
     type,
-    onClick
+    onClick,
+    disabled
 }) => (
     <button
-    className="font-medium text-slate-800 bg-orange-500 hover:bg-orange-400 hover:shadow-md hover:shadow-orange-400/40 px-4 py-1 rounded-md"
+    disabled={disabled}
+    className="font-medium text-slate-800 
+    bg-orange-500 hover:bg-orange-400 disabled:bg-orange-500/60 hover:shadow-md disabled:shadow-none hover:shadow-orange-400/40 px-4 py-1 rounded-md"
     type={type}
     onClick={onClick}
     >{text}</button>
@@ -18,7 +21,8 @@ const SecondaryButton = ({
     onClick
 }) => (
     <button
-    className="text-teal-500 outline outline-1 hover:outline-2 outline-teal-500 hover:shadow-md hover:shadow-teal-500/40 px-4 py-1 rounded-md"
+    className="text-teal-500 outline outline-1 
+    hover:outline-2 outline-teal-500 hover:shadow-md hover:shadow-teal-500/40 px-4 py-1 rounded-md"
     type={type}
     onClick={onClick}
     >{text}</button>
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -11,38 +11,76 @@ const RegisterForm = ({
         [name, setName] = useState(""),
         [password, setPassword] = useState(""),
         [passwordConfirm, setPasswordConfirm] = useState(""),
-        [code, setCode] = useState("")
+        [code, setCode] = useState(""),
+        
+        [validUsername, setValidUsername] = useState(false),
+        [validName, setValidName] = useState(false),
+        [validPwd, setValidPwd] = useState(false),
+        [validPwdConfirm, setValidPwdConfirm] = useState(false),
+        [validCode, setValidCode] = useState(false)
+
+    const isValid = () => {
+        return validUsername && validName && validPwd && validPwdConfirm && validCode
+    }
 
     const submitHandler = (event) => {
         event.preventDefault()
-        handleSubmit({
-            username, name, password, code
-        })
+        if (isValid()) {
+            handleSubmit({
+                username, name, password, code
+            })
+        }
+    }
+
+    const validateUsername = (name) => {
+        if (name && name.length > 2 && name.length <= 20) {
+            setValidUsername(true)
+            return ""
+        } else {
+            setValidUsername(false)
+            return "Should be between 3 and 20 characters"
+        }
     }
 
     const validateName = (name) => {
-        if (name && name.length > 2 && name.length < 12) {
+        if (name && name.length > 2 && name.length <= 20) {
+            setValidName(true)
             return ""
         } else {
-            return "Should be between 3 and 11 characters"
+            setValidUsername(false)
+            return "Should be between 3 and 20 characters"
         }
     }
 
     const validatePassword = (password) => {
         if (password && password.length > 5 && password.length < 40) {
+            setValidPwd(true)
             return ""
         } else {
+            setValidPwd(false)
             return "Should be longer than 5 characters"
         }
     }
 
     const validatePasswordConfirm = (passwordConfirm) => {
         if (passwordConfirm !== password) {
+            setValidPwdConfirm(false)
             return "Passwords do not match!"
         } else {
+            setValidPwdConfirm(true)
             return ""
         }
     }
+
+    const validateCode = (code) => {
+        setValidCode(code !== "")
+        if (!validCode) {
+            return "Cannot be empty"
+        }
+        return ""
+    }
+
+    console.log(isValid())
 
     return (
         <div className="flex flex-row justify-center py-2 px-6 w-full">
@@ -56,7 +94,7 @@ const RegisterForm = ({
                     <div className="flex flex-row">
                         <InputField type="text" placeholder="username" value={username} 
                         onChange={(event) => {setUsername(event.target.value)}} 
-                        validation={validateName}/>
+                        validation={validateUsername}/>
                     </div>
                     <div className="flex flex-row mb-4">
                         <InputField type="text" placeholder="name" value={name} 
@@ -76,9 +114,10 @@ const RegisterForm = ({
                     </div>
                     <div className="flex flex-row mb-4">
                         <InputField type="number" placeholder="code" value={code} 
-                        onChange={(event) => {setCode(event.target.value)}} />
+                        onChange={(event) => {setCode(event.target.value)}}
+                        validation={validateCode} />
                     </div>
-                    <PrimaryButton type="submit" text="Register"/>
+                    <PrimaryButton type="submit" text="Register" disabled={!isValid()}/>
                 </form>
             </Surface>
         </div>
