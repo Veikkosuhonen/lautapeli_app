becee659b6ad5f6112530c78fdffdba07a59ef39
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-01-05 19:18:01 +0200

    Removed unused import

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { NavLink, useNavigate } from "react-router-dom"
-import { Disclosure, Menu } from "@headlessui/react"
+import { Disclosure } from "@headlessui/react"
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import { SecondaryButton } from "./Buttons"
 
