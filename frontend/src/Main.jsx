6bb69a92a25a951c5c23d5e6c50938266d244fdc
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-01-07 17:40:39 +0200

    removed unused var

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 
 import Boardgames from './components/Boardgames'
 import SelectedBoardgame from './components/SelectedBoardgame'
