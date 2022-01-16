21b4de2b24af90b4e6892451ffb413b1c5629915
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-01-16 15:52:08 +0200

    Improved home page

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Activities from "../components/Activities"
 import HeroSection from "../components/HeroSection"
-import { NavLink } from "react-router-dom"
 
 const Home = ({
     user, activities, users, boardgames
@@ -16,27 +15,34 @@ const Home = ({
                 ? <h1>Greetings, {user.name}</h1>
                 : <h1>Welcome to the great lautapeli app</h1>
                 }
-                { user && <>
-                <span className="text-lg font-base text-slate-300">{user ? "Here is the latest status report:" : "Currently we have"}</span>
-                <div className="flex flex-row flex-wrap gap-2 text-slate-200 font-medium text-base sm:text-lg">
-                    <span className="rounded border border-slate-700 p-2">
-                        {users ? users.length : "?"} users
-                    </span>
-                    <span className="rounded border border-slate-700 p-2">
-                        {boardgames ? boardgames.length : "?"} boardgames
-                    </span>
-                    <span className="rounded border border-slate-700 p-2">
-                        {playSessions} games played
-                    </span>
-                </div>
-                </>}
             </HeroSection>
-            
-            <div className="flex flex-row justify-center pt-16 sm:pt-24 md:pt-40 lg:pt-24">
+
+            { user && 
+            <div className="px-2 pt-12 sm:pt-20 md:pt-32 lg:pt-24 flex flex-row justify-center">
+                <div>
+                    <span className="text-lg font-base text-slate-400">{user ? "Here is the latest status report:" : "Currently we have"}</span>
+                    <div className="flex flex-row flex-wrap gap-2 text-slate-200 font-medium text-base sm:text-lg">
+                        <span className="select-none rounded border border-slate-700 p-2">
+                            {users ? users.length : "?"} users
+                        </span>
+                        <a href="/boardgames" className="rounded border border-slate-700 hover:bg-slate-800 p-2">
+                            {boardgames ? boardgames.length : "?"} boardgames
+                        </a>
+                        <span className="select-none rounded border border-slate-700 p-2">
+                            {playSessions} games played
+                        </span>
+                    </div>
+                </div>
+            </div>
+            }
+
+            <div className="flex flex-row justify-center pt-16">
             {user ?
+            <>
                 <div className="w-full sm:w-1/2">
                     <Activities activities={activities} itemsPerPage={5} />
-                </div> : 
+                </div>
+            </> : 
                 <div className="flex flex-col">
                     <h1 className="text-xl font-light text-slate-200 text-center">To get started,</h1>
                     <div className="text-lg font-medium text-slate-300">
