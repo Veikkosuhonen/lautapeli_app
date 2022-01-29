4ad683a00da1a18a330c6ec2ce5dd05a0e5413c1
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-01-29 14:00:26 +0200

    Registration form uses formik

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,74 +1,40 @@
-import React, { useState } from "react"
+import { Formik } from 'formik';
+import * as Yup from "yup"
 import { PrimaryButton } from "./util/Buttons"
 import Surface from "./util/Surface"
-import InputField from "./util/InputField"
-import { validation } from "../util/validation"
 import { NavLink } from "react-router-dom"
+import FormikInputField from './util/FormikInputField';
 
 const RegisterForm = ({
     handleSubmit
-}) => {
-    const [username, setUsername] = useState(""),
-        [name, setName] = useState(""),
-        [password, setPassword] = useState(""),
-        [passwordConfirm, setPasswordConfirm] = useState(""),
-        [code, setCode] = useState(""),
-        
-        [validUsername, setValidUsername] = useState(false),
-        [validName, setValidName] = useState(false),
-        [validPwd, setValidPwd] = useState(false),
-        [validPwdConfirm, setValidPwdConfirm] = useState(false),
-        [validCode, setValidCode] = useState(false)
-
-    const isValid = () => {
-        return validUsername && validName && validPwd && validPwdConfirm && validCode
-    }
-
-    const submitHandler = (event) => {
-        event.preventDefault()
-        if (isValid()) {
-            handleSubmit({
-                username, name, password, code
-            })
-        }
-    }
-
-    const validateUsername = validation(
-        setValidUsername,
-        (username) => username && username.length > 2 && username.length <= 20,
-        "Should be between 3 and 20 characters"
-    )
-
-    const validateName = validation(
-        setValidName,
-        (username) => username && username.length > 2 && username.length <= 20,
-        "Should be between 3 and 20 characters"
-    )
-    
-    const validatePassword = validation(
-        setValidPwd,
-        (password) => password && password.length > 5 && password.length <= 40,
-        "Should be between 3 and 20 characters"
-    )
-
-    const validatePasswordConfirm = validation(
-        setValidPwdConfirm,
-        (passwordConfirm) => passwordConfirm === password,
-        "Passwords do not match!"
-    )
-
-    const validateCode = validation(
-        setValidCode,
-        (code) => code !== "",
-        "Must not be empty"
-    )
-
-    return (
-        <div className="flex flex-row justify-center py-2 px-6 w-full">
-            <Surface className="w-4/5 sm:w-full sm:basis-1/2 md:basis-1/2 lg:basis-1/4">
-                <form 
-                onSubmit={submitHandler}
-                className="flex flex-col gap-4 p-2 py-4">
+}) => (
+    <div className="flex flex-row justify-center py-2 px-6 w-full">
+        <Surface className="w-4/5 sm:w-full sm:basis-1/2 md:basis-1/2 lg:basis-1/4">
+            <Formik
+                initialValues={{
+                    username: "",
+                    name: "",
+                    password: "",
+                    passwordConfirm: "",
+                    code: "",
+                }}
+                initialErrors={{
+                    username: "Required",
+                    name: "Required",
+                    passoword: "Required",
+                    passwordConfirm: "Required",
+                    code: "Required",
+                }}
+                validationSchema={Yup.object({
+                    username: Yup.string().min(3, "Min length is 3").max(20, "Min length is 20").required("Required"),
+                    name: Yup.string().min(3, "Min length is 3").max(20, "Max length is 20").required("Required"),
+                    password: Yup.string().min(5, "Min length is 5").max(20, "Max length is 20").required("Required"),
+                    passwordConfirm: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
+                    code: Yup.number().integer().min(1000).required("Required"),
+                })}
+            >
+            {formik => (
+                <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4 p-2 py-4">
                     <div className="flex flex-row pb-4 gap-x-2 items-center">
                         <h1 className="text-lg text-slate-100 font-medium mr-auto">Register</h1>
                         <span className="hidden md:block text-slate-300">or</span>
@@ -77,40 +43,48 @@ const RegisterForm = ({
                             <span className="block sm:hidden text-indigo-400 font-medium hover:text-indigo-300">login</span>
                         </NavLink>
                     </div>
-                    <div className="flex flex-row">
-                        <InputField type="text" placeholder="username" value={username} 
-                        onChange={(event) => {setUsername(event.target.value)}} 
+                    <FormikInputField 
+                        label="Username"
+                        name="username"
+                        placeholder="Username"
                         autoComplete="off"
-                        validation={validateUsername}/>
-                    </div>
-                    <div className="flex flex-row mb-4">
-                        <InputField type="text" placeholder="name" value={name} 
-                        onChange={(event) => {setName(event.target.value)}}
+                    />
+                    <FormikInputField 
+                        label="Name"
+                        name="name"
+                        placeholder="Name"
                         autoComplete="off"
-                        validation={validateName}/>
-                    </div>
-                    <div className="flex flex-row">
-                        <InputField type="password" placeholder="password" autoComplete="new-password" 
-                        value={password} 
-                        onChange={(event) => {setPassword(event.target.value)}}
-                        validation={validatePassword}/>
-                    </div>
-                    <div className="flex flex-row mb-4">
-                        <InputField type="password" placeholder="confirm password" value={passwordConfirm} 
-                        onChange={(event) => {setPasswordConfirm(event.target.value)}}
-                        validation={validatePasswordConfirm} />
-                    </div>
-                    <div className="flex flex-row mb-4">
-                        <InputField type="number" placeholder="code" value={code} 
-                        onChange={(event) => {setCode(event.target.value)}}
+                    />
+                    <div />
+                    <FormikInputField 
+                        label="Password"
+                        name="password"
+                        type="password"
+                        placeholder="Password"
                         autoComplete="off"
-                        validation={validateCode} />
-                    </div>
-                    <PrimaryButton type="submit" content="Register" disabled={!isValid()}/>
+                    />
+                    <FormikInputField 
+                        label="Confirm Password"
+                        name="passwordConfirm"
+                        type="password"
+                        placeholder="Confirm password"
+                        autoComplete="off"
+                    />
+                    <div />
+                    <FormikInputField 
+                        label="Code"
+                        name="code"
+                        type="number"
+                        placeholder="Code"
+                        autoComplete="off"
+                    />
+                    <PrimaryButton content={"Register"} type={"submit"} disabled={!formik.isValid}/>
                 </form>
-            </Surface>
-        </div>
-    )
-}
+            )}
+            </Formik>
+        </Surface>
+    </div>
+)
+
 
 export default RegisterForm
